3374a86a8f9e48984bbdbdc5ee761dfacec8bc8f
Achyut <achyuttripathi06@gmail.com>
2025-11-21 00:15:03 +0530

    handeling endpoints

---
+++ src/main/java/com/atripathi/journalApp/Controller/JournalEntryControllerV2.java
@@ -0,0 +1,54 @@
+package com.atripathi.journalApp.Controller;
+
+
+import com.atripathi.journalApp.entity.JournalEntry;
+import com.atripathi.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/journal") // add this mapping to the whole class
+public class JournalEntryControllerV2 {
+
+    @Autowired
+    private JournalEntryService journalEntryService;
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return journalEntryService.getAll();
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry) {
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+        return myEntry;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myid) {
+        return journalEntryService.findById(myid).orElse(null);
+    }
+
+
+    @DeleteMapping("id/{myId}")
+    public boolean deleteJournalEntryById(@PathVariable ObjectId myid) {
+       journalEntryService.deleteById(myid);
+       return true;
+    }
+
+    @PutMapping("/id/{id}")
+    public JournalEntry updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry){
+        JournalEntry old = journalEntryService.findById(id).orElse(null);
+        if (old != null) {
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent());
+        }
+        journalEntryService.saveEntry(old);
+        return old;
+    }
+}
